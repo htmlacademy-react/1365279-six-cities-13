3c74628829415dd7e54adb700a82509de5a7470e
Daria <daria.mart21@gmail.com>
2023-07-15 12:17:24 +0300

    Добавлены компоненты header и footer

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import OfferCard from '../../components/offer-card/offer-card';
 import { CITIES } from '../../const';
 import mockOffer from '../../mocks/offers';
+import Header from '../../components/header/header';
 
 type MainPageProps = {
 	offersCount: number;
@@ -12,44 +13,7 @@ function MainPage({offersCount}: MainPageProps): JSX.Element {
 	const offers = Array.from({length: offersCount}, mockOffer);
 	return (
 		<div className="page page--gray page--main">
-			<header className="header">
-				<div className="container">
-					<div className="header__wrapper">
-						<div className="header__left">
-							<a className="header__logo-link header__logo-link--active">
-								<img
-									className="header__logo"
-									src="img/logo.svg"
-									alt="6 cities logo"
-									width={81}
-									height={41}
-								/>
-							</a>
-						</div>
-						<nav className="header__nav">
-							<ul className="header__nav-list">
-								<li className="header__nav-item user">
-									<a
-										className="header__nav-link header__nav-link--profile"
-										href="#"
-									>
-										<div className="header__avatar-wrapper user__avatar-wrapper"></div>
-										<span className="header__user-name user__name">
-											Oliver.conner@gmail.com
-										</span>
-										<span className="header__favorite-count">3</span>
-									</a>
-								</li>
-								<li className="header__nav-item">
-									<a className="header__nav-link" href="#">
-										<span className="header__signout">Sign out</span>
-									</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</div>
-			</header>
+			<Header />
 			<main className="page__main page__main--index">
 				<h1 className="visually-hidden">Cities</h1>
 				<div className="tabs">
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,26 +1,11 @@
 import {Link} from 'react-router-dom';
 import styles from './not-found-page.module.css';
+import Header from '../../components/header/header';
 
 function NotFoundPage() : JSX.Element {
 	return (
 		<div className="page page--gray">
-			<header className="header">
-				<div className="container">
-					<div className="header__wrapper">
-						<div className="header__left">
-							<a className="header__logo-link header__logo-link--active">
-								<img
-									className="header__logo"
-									src="img/logo.svg"
-									alt="6 cities logo"
-									width={81}
-									height={41}
-								/>
-							</a>
-						</div>
-					</div>
-				</div>
-			</header>
+			<Header withNavigation={false} />
 			<div className='container'>
 				<h1>Ошибка 404. Страница не существует.</h1>
 				<Link to="/" className={styles.link}>Вернуться на главную</Link>
