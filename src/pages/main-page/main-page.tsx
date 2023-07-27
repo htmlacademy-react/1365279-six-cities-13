66e0ebf92efb40f62b4158796305121d50445d2b
Daria <daria.mart21@gmail.com>
2023-07-27 12:43:57 +0300

    В main-page добавлен вариант отрисовки без оферов

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -36,7 +36,7 @@ function MainPage({offers}: MainPageProps): JSX.Element {
 				<title>6 cities</title>
 			</Helmet>
 			<Header />
-			<main className="page__main page__main--index">
+			<main className={classNames('page__main', 'page__main--index', {'page__main--index-empty': offers.length === 0})}>
 				<h1 className="visually-hidden">Cities</h1>
 				<div className="tabs">
 					<section className="locations container">
@@ -62,46 +62,61 @@ function MainPage({offers}: MainPageProps): JSX.Element {
 					</section>
 				</div>
 				<div className="cities">
-					<div className="cities__places-container container">
-						<section className="cities__places places">
-							<h2 className="visually-hidden">Places</h2>
-							<b className="places__found">{offersByCities[activeCity].length} places to stay in {activeCity}</b>
-							<form className="places__sorting" action="#" method="get">
-								<span className="places__sorting-caption">Sort by</span>{' '}
-								<span className="places__sorting-type" tabIndex={0}>
-									Popular
-									<svg className="places__sorting-arrow" width={7} height={4}>
-										<use xlinkHref="#icon-arrow-select" />
-									</svg>
-								</span>
-								<ul className="places__options places__options--custom places__options--opened">
-									<li
-										className="places__option places__option--active"
-										tabIndex={0}
-									>
+					{offers.length > 0 ? (
+						<div className="cities__places-container container">
+							<section className="cities__places places">
+								<h2 className="visually-hidden">Places</h2>
+								<b className="places__found">{offersByCities[activeCity].length} places to stay in {activeCity}</b>
+								<form className="places__sorting" action="#" method="get">
+									<span className="places__sorting-caption">Sort by</span>{' '}
+									<span className="places__sorting-type" tabIndex={0}>
 										Popular
-									</li>
-									<li className="places__option" tabIndex={0}>
-										Price: low to high
-									</li>
-									<li className="places__option" tabIndex={0}>
-										Price: high to low
-									</li>
-									<li className="places__option" tabIndex={0}>
-										Top rated first
-									</li>
-								</ul>
-							</form>
-							<div className="cities__places-list places__list tabs__content">
-								{offersByCities[activeCity].map((offer) => (
-									<OfferCard {...offer} key={offer.id} onMouseEnter={() => setActiveOffer(offer)} onMouseLeave={() => setActiveOffer(undefined)} />
-								))}
+										<svg className="places__sorting-arrow" width={7} height={4}>
+											<use xlinkHref="#icon-arrow-select" />
+										</svg>
+									</span>
+									<ul className="places__options places__options--custom places__options--opened">
+										<li
+											className="places__option places__option--active"
+											tabIndex={0}
+										>
+											Popular
+										</li>
+										<li className="places__option" tabIndex={0}>
+											Price: low to high
+										</li>
+										<li className="places__option" tabIndex={0}>
+											Price: high to low
+										</li>
+										<li className="places__option" tabIndex={0}>
+											Top rated first
+										</li>
+									</ul>
+								</form>
+								<div className="cities__places-list places__list tabs__content">
+									{offersByCities[activeCity].map((offer) => (
+										<OfferCard {...offer} key={offer.id} onMouseEnter={() => setActiveOffer(offer)} onMouseLeave={() => setActiveOffer(undefined)} />
+									))}
+								</div>
+							</section>
+							<div className="cities__right-section">
+								<Map city={currentCity[0].city} points={currentCity} activeOffer={activeOffer} />
 							</div>
-						</section>
-						<div className="cities__right-section">
-							<Map city={currentCity[0].city} points={currentCity} activeOffer={activeOffer} />
 						</div>
-					</div>
+					) : (
+						<div className="cities__places-container cities__places-container--empty container">
+							<section className="cities__no-places">
+								<div className="cities__status-wrapper tabs__content">
+									<b className="cities__status">No places to stay available</b>
+									<p className="cities__status-description">
+										We could not find any property available at the moment in
+										Dusseldorf
+									</p>
+								</div>
+							</section>
+							<div className="cities__right-section" />
+						</div>
+					)}
 				</div>
 			</main>
 		</div>
