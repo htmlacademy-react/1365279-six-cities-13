ff0e287c1ce837c3721e1208a8cc0044bfe2eebf
Little-red-panda <60811698+Little-red-panda@users.noreply.github.com>
2023-08-04 09:31:03 +0300

    Больше подробностей (часть 2) (#6)
    * Добавлены компоненты reviews-list, reviews-item.
    Сгенерированы моковые отзывы
    
    * Добавлены карточки для объявлений неподалеку

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,7 +8,7 @@ import OfferPage from '../../pages/offer-page/offer-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import mockOffers from '../../mocks/offers';
-
+import mockReviews from '../../mocks/reviews';
 
 function App(): JSX.Element {
 	return (
@@ -22,9 +22,7 @@ function App(): JSX.Element {
 					<Route
 						path={AppRoute.Login}
 						element={
-							<PublicRoute
-								status={AuthorizationStatus.NoAuth}
-							>
+							<PublicRoute status={AuthorizationStatus.NoAuth}>
 								<LoginPage />
 							</PublicRoute>
 						}
@@ -32,21 +30,16 @@ function App(): JSX.Element {
 					<Route
 						path={AppRoute.Favorites}
 						element={
-							<PrivateRoute
-								status={AuthorizationStatus.Auth}
-							>
+							<PrivateRoute status={AuthorizationStatus.Auth}>
 								<FavoritesPage offers={mockOffers} />
 							</PrivateRoute>
 						}
 					/>
 					<Route
 						path={`${AppRoute.Offer}/:offerId`}
-						element={<OfferPage />}
+						element={<OfferPage reviews={mockReviews} offers={mockOffers} />}
 					/>
-					<Route
-						path='*'
-						element={<NotFoundPage />}
-					/>
+					<Route path="*" element={<NotFoundPage />} />
 				</Routes>
 			</BrowserRouter>
 		</HelmetProvider>
--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -1,9 +1,28 @@
-import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import ReviewsForm from '../../components/reviews-form/reviews-form';
+import ReviewsList from '../../components/reviews-list/reviews-list';
+import { Review } from '../../types/review';
+import { ServerOffer } from '../../types/offer';
+import LeafletMap from '../../components/leaflet-map/leaflet-map';
+import OfferCard from '../../components/offer-card/offer-card';
+import { SetStateAction, useState } from 'react';
 
-function OfferPage(): JSX.Element {
+type ReviewsListProps = {
+	reviews: Review[];
+	offers: ServerOffer[];
+};
+
+function OfferPage({ reviews, offers }: ReviewsListProps): JSX.Element {
+	const nearbyOffers = offers.slice(0, 3);
+	const [activeOffer, setActiveOffer] = useState<ServerOffer | undefined>(
+		undefined
+	);
+	const handleActiveOfferChange = (
+		offer: SetStateAction<ServerOffer | undefined>
+	) => {
+		setActiveOffer(offer);
+	};
 	return (
 		<div className="page">
 			<Helmet>
@@ -65,7 +84,7 @@ function OfferPage(): JSX.Element {
 							</div>
 							<div className="offer__name-wrapper">
 								<h1 className="offer__name">
-              Beautiful &amp; luxurious studio at great location
+									Beautiful &amp; luxurious studio at great location
 								</h1>
 								<button className="offer__bookmark-button button" type="button">
 									<svg className="offer__bookmark-icon" width={31} height={33}>
@@ -82,12 +101,14 @@ function OfferPage(): JSX.Element {
 								<span className="offer__rating-value rating__value">4.8</span>
 							</div>
 							<ul className="offer__features">
-								<li className="offer__feature offer__feature--entire">Apartment</li>
+								<li className="offer__feature offer__feature--entire">
+									Apartment
+								</li>
 								<li className="offer__feature offer__feature--bedrooms">
-              3 Bedrooms
+									3 Bedrooms
 								</li>
 								<li className="offer__feature offer__feature--adults">
-              Max 4 adults
+									Max 4 adults
 								</li>
 							</ul>
 							<div className="offer__price">
@@ -126,206 +147,53 @@ function OfferPage(): JSX.Element {
 								</div>
 								<div className="offer__description">
 									<p className="offer__text">
-                A quiet cozy and picturesque that hides behind a a river by the
-                unique lightness of Amsterdam. The building is green and from
-                18th century.
+										A quiet cozy and picturesque that hides behind a a river by
+										the unique lightness of Amsterdam. The building is green and
+										from 18th century.
 									</p>
 									<p className="offer__text">
-                An independent House, strategically located between Rembrand
-                Square and National Opera, but where the bustle of the city
-                comes to rest in this alley flowery and colorful.
+										An independent House, strategically located between Rembrand
+										Square and National Opera, but where the bustle of the city
+										comes to rest in this alley flowery and colorful.
 									</p>
 								</div>
 							</div>
 							<section className="offer__reviews reviews">
 								<h2 className="reviews__title">
-              Reviews · <span className="reviews__amount">1</span>
+									Reviews ·{' '}
+									<span className="reviews__amount">{reviews.length}</span>
 								</h2>
-								<ul className="reviews__list">
-									<li className="reviews__item">
-										<div className="reviews__user user">
-											<div className="reviews__avatar-wrapper user__avatar-wrapper">
-												<img
-													className="reviews__avatar user__avatar"
-													src="img/avatar-max.jpg"
-													width={54}
-													height={54}
-													alt="Reviews avatar"
-												/>
-											</div>
-											<span className="reviews__user-name">Max</span>
-										</div>
-										<div className="reviews__info">
-											<div className="reviews__rating rating">
-												<div className="reviews__stars rating__stars">
-													<span style={{ width: '80%' }} />
-													<span className="visually-hidden">Rating</span>
-												</div>
-											</div>
-											<p className="reviews__text">
-                    A quiet cozy and picturesque that hides behind a a river by
-                    the unique lightness of Amsterdam. The building is green and
-                    from 18th century.
-											</p>
-											<time className="reviews__time" dateTime="2019-04-24">
-                    April 2019
-											</time>
-										</div>
-									</li>
-								</ul>
+								<ReviewsList reviews={reviews} />
 								<ReviewsForm />
 							</section>
 						</div>
 					</div>
-					<section className="offer__map map" />
+					<LeafletMap
+						city={nearbyOffers[0].city}
+						points={nearbyOffers}
+						activeOffer={activeOffer}
+						block={'offer'}
+					/>
 				</section>
 				<div className="container">
 					<section className="near-places places">
 						<h2 className="near-places__title">
-          Other places in the neighbourhood
+							Other places in the neighborhood
 						</h2>
 						<div className="near-places__list places__list">
-							<article className="near-places__card place-card">
-								<div className="near-places__image-wrapper place-card__image-wrapper">
-									<Link to="#">
-										<img
-											className="place-card__image"
-											src="img/room.jpg"
-											width={260}
-											height={200}
-											alt="Place image"
-										/>
-									</Link>
-								</div>
-								<div className="place-card__info">
-									<div className="place-card__price-wrapper">
-										<div className="place-card__price">
-											<b className="place-card__price-value">€80</b>
-											<span className="place-card__price-text">/&nbsp;night</span>
-										</div>
-										<button
-											className="place-card__bookmark-button place-card__bookmark-button--active button"
-											type="button"
-										>
-											<svg
-												className="place-card__bookmark-icon"
-												width={18}
-												height={19}
-											>
-												<use xlinkHref="#icon-bookmark" />
-											</svg>
-											<span className="visually-hidden">In bookmarks</span>
-										</button>
-									</div>
-									<div className="place-card__rating rating">
-										<div className="place-card__stars rating__stars">
-											<span style={{ width: '80%' }} />
-											<span className="visually-hidden">Rating</span>
-										</div>
-									</div>
-									<h2 className="place-card__name">
-										<Link to="#">Wood and stone place</Link>
-									</h2>
-									<p className="place-card__type">Private room</p>
-								</div>
-							</article>
-							<article className="near-places__card place-card">
-								<div className="near-places__image-wrapper place-card__image-wrapper">
-									<Link to="#">
-										<img
-											className="place-card__image"
-											src="img/apartment-02.jpg"
-											width={260}
-											height={200}
-											alt="Place image"
-										/>
-									</Link>
-								</div>
-								<div className="place-card__info">
-									<div className="place-card__price-wrapper">
-										<div className="place-card__price">
-											<b className="place-card__price-value">€132</b>
-											<span className="place-card__price-text">/&nbsp;night</span>
-										</div>
-										<button
-											className="place-card__bookmark-button button"
-											type="button"
-										>
-											<svg
-												className="place-card__bookmark-icon"
-												width={18}
-												height={19}
-											>
-												<use xlinkHref="#icon-bookmark" />
-											</svg>
-											<span className="visually-hidden">To bookmarks</span>
-										</button>
-									</div>
-									<div className="place-card__rating rating">
-										<div className="place-card__stars rating__stars">
-											<span style={{ width: '80%' }} />
-											<span className="visually-hidden">Rating</span>
-										</div>
-									</div>
-									<h2 className="place-card__name">
-										<Link to="#">Canal View Prinsengracht</Link>
-									</h2>
-									<p className="place-card__type">Apartment</p>
-								</div>
-							</article>
-							<article className="near-places__card place-card">
-								<div className="place-card__mark">
-									<span>Premium</span>
-								</div>
-								<div className="near-places__image-wrapper place-card__image-wrapper">
-									<Link to="#">
-										<img
-											className="place-card__image"
-											src="img/apartment-03.jpg"
-											width={260}
-											height={200}
-											alt="Place image"
-										/>
-									</Link>
-								</div>
-								<div className="place-card__info">
-									<div className="place-card__price-wrapper">
-										<div className="place-card__price">
-											<b className="place-card__price-value">€180</b>
-											<span className="place-card__price-text">/&nbsp;night</span>
-										</div>
-										<button
-											className="place-card__bookmark-button button"
-											type="button"
-										>
-											<svg
-												className="place-card__bookmark-icon"
-												width={18}
-												height={19}
-											>
-												<use xlinkHref="#icon-bookmark" />
-											</svg>
-											<span className="visually-hidden">To bookmarks</span>
-										</button>
-									</div>
-									<div className="place-card__rating rating">
-										<div className="place-card__stars rating__stars">
-											<span style={{ width: '100%' }} />
-											<span className="visually-hidden">Rating</span>
-										</div>
-									</div>
-									<h2 className="place-card__name">
-										<Link to="#">Nice, cozy, warm big bed apartment</Link>
-									</h2>
-									<p className="place-card__type">Apartment</p>
-								</div>
-							</article>
+							{nearbyOffers.map((offer) => (
+								<OfferCard
+									{...offer}
+									key={offer.id}
+									onMouseEnter={() => handleActiveOfferChange(offer)}
+									onMouseLeave={() => handleActiveOfferChange(undefined)}
+								/>
+							))}
 						</div>
 					</section>
 				</div>
 			</main>
 		</div>
-
 	);
 }
 
