b66024b3bb5ee49048638ff588789590d61bd22b
Daria <daria.mart21@gmail.com>
2023-08-11 17:31:54 +0300

    Добавлен спиннер

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -3,7 +3,7 @@ import { AppDispatch, State } from '../types/state';
 import { AxiosInstance } from 'axios';
 import { ServerOffer } from '../types/offer';
 import { APIRoute } from '../const';
-import { loadOffers } from './actions';
+import { loadOffers, setOffersLoadingStatus } from './actions';
 
 export const fetchOffersAction = createAsyncThunk<
 	void,
@@ -14,6 +14,8 @@ export const fetchOffersAction = createAsyncThunk<
 		extra: AxiosInstance;
 	}
 >('data/fetchOffersAction', async (_arg, { dispatch, extra: api }) => {
+	dispatch(setOffersLoadingStatus(true));
 	const { data } = await api.get<ServerOffer[]>(APIRoute.Offers);
+	dispatch(setOffersLoadingStatus(false));
 	dispatch(loadOffers(data));
 });
